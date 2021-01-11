bfe2b3e4ff1d4e9e15a7d309c211634a384a4f9a
inwoo7233 <inwoo7233@gmail.com>
2021-01-11 16:04:21 +0900

    nest-project

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,13 +1,13 @@
-import { IsNumber, IsOptional, IsString } from "class-validator";
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateMovieDto {
-    @IsString()
-    readonly title: string;
+  @IsString()
+  readonly title: string;
 
-    @IsNumber()
-    readonly year: number;
+  @IsNumber()
+  readonly year: number;
 
-    @IsOptional()
-    @IsString({ each: true })
-    readonly genres: string[];
+  @IsOptional()
+  @IsString({ each: true })
+  readonly genres: string[];
 }
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,41 +1,43 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Patch,
+  Body,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
-import { Movie } from './entities/movie.entity';
-import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
-    constructor(private readonly moviesService: MoviesService) {
-    }
+  constructor(private readonly moviesService: MoviesService) {}
 
-    @Get()
-    getAll(): Movie[] {
-        return this.moviesService.getAll();
-    }
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
 
-    @Get("search")
-    search(@Query("year") searchingYear: string) {
-        return `We are searching for a movie made after: ${searchingYear}`;
-    }
+  @Get(':id')
+  getOne(@Param('id') movieId: number): Movie {
+    return this.moviesService.getOne(movieId);
+  }
 
-    @Get(":id")
-    getOne(@Param("id") movieId: number): Movie {
-        return this.moviesService.getOne(movieId);
-    }
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
 
-    @Post()
-    create(@Body() movieData: CreateMovieDto) {
-        return this.moviesService.create(movieData);
-    }
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
+  }
 
-    @Delete(":id")
-    remove(@Param("id") movieId: number) {
-        return this.moviesService.deleteOne(movieId);
-    }
-
-    @Patch(':id')
-    patch(@Param("id") movieId: number, @Body() updateData: UpdateMovieDto) {
-        return this.moviesService.update(movieId, updateData);
-    }
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
+  }
 }
